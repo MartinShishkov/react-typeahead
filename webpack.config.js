4dbe72f83a126e5ceee35d830b551cab6e2be7d8
Martin Shishkov <martin.shishkov@hotmail.com>
2019-07-14 17:15:43 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+var path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        "app": "./src/App.tsx",
+    },
+    output: {
+        publicPath: "/build/js/",
+        path: path.resolve(__dirname, 'build/js'),
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                use: "awesome-typescript-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+    }
+};
